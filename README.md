9d076f3da9e8da069d15306ec34e8928826d9f42
hiteshbhatia3559 <31386280+hiteshbhatia3559@users.noreply.github.com>
2019-03-13 03:06:20 +0530

    All done, priority project completed awaiting revisions

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ Problem statement
 Hi, I want to integrate aircraft flight data onto a website. Can you help me do this/is this something you would know how to do? I am looking to get it done today as well and am willing to pay a bit more for that. https://aviation-edge.com/aviation-api-list/. 
 I am looking to scrape flight data when a user types in their flight number and I want it to tell them whether or not they are flying a 737 8 MAX, so like an If statement
 ```
+
+Running instructions
+``` 
+0. Install Python 3.5
+1. Download this repo as a zip file and extract
+2. Run python main.py
+3. Input your flight number
+4. If Yes pops up, you are flying on a 737 Max 8
+5. If No pops up, there is a high chance that you are not flying in a Max 8 (subject to addition of data and schedule changes)
+6. To quit, type "quit" and the program exits
+```
--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import requests
 import json
-import ast
 
 api_key = "cb357c-3cdad8"
-not_allowed = ['B37M','B38M','B39M','B3JM','B738']
 
 def get_answer(flight_number):
     url = "https://aviation-edge.com/get.php"
 
     response = json.loads(requests.get(
-        url=" http://aviation-edge.com/v2/public/flights?key=" + api_key + "&flightIcao=" + flight_number).text)
+        url=" http://aviation-edge.com/v2/public/flights?key=" + api_key + "&aircraftIcao=" + flight_number).text)
 
     return response
 
-# Code outputs the type of aircraft
+while 1:
+    input_variable = input("What is your flight number?")
+    if input_variable.lower() != "quit":
+        found = "NO"
+        for i in range(0,10):
+            answer = get_answer("B738")
+            for item in answer:
+                # if input_variable in [item['flight']['iataNumber'],item['flight']['icaoNumber'],item['aircraft']['regNumber']]:
+                #     print("YES")
+                if input_variable == item['flight']['iataNumber']:
+                    found = "YES"
+                if input_variable == item['flight']['icaoNumber']:
+                    found = "YES"
+                if input_variable == item['aircraft']['regNumber']:
+                    found = "YES"
+        print(found)
+    else:
+        print("Quitting")
+        exit()
 
-if __name__ == "__main__":
-    while 1:
-        # flight_number = input("What is your flight number?")
-        flight_number = "BA2490"
-        if flight_number.lower() != "quit":
-            print(get_answer(flight_number))
-        else: exit()
+
+
+
+
+
