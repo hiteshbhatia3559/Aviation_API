49b9c82d2ae1fea474be897bf8a28236013980ee
hiteshbhatia3559 <31386280+hiteshbhatia3559@users.noreply.github.com>
2019-03-13 02:17:35 +0530

    Added aircraft, now coding responses

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ import requests
 import json
 import ast
 
-api_key = 'cb357c-3cdad8'
-flight_number = "6E263"
-url = "https://aviation-edge.com/get.php"
+api_key = "cb357c-3cdad8"
+not_allowed = ['B37M','B38M','B39M','B3JM','B738']
 
-response = json.loads(requests.get(url=" http://aviation-edge.com/v2/public/flights?key=cb357c-3cdad8&flightIata=6E263").text)
+def get_answer(flight_number):
+    url = "https://aviation-edge.com/get.php"
 
-print(response[0]['aircraft']['icaoCode'])
+    response = json.loads(requests.get(
+        url=" http://aviation-edge.com/v2/public/flights?key=" + api_key + "&flightIata=" + flight_number).text)
+
+    print(response)
+
+# Code outputs the type of aircraft
+
+if __name__ == "__main__":
+    while 1:
+        flight_number = input("What is your flight number?")
+        if flight_number.lower() != "quit":
+            get_answer(flight_number)
+        else: exit()
